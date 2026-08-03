6aa2cd67c3f74d03ca12dc3ab688182ea11c7089
Vrushabh Gamare <vrushabhgamare@Vrushabhs-MacBook-Air-2.local>
2026-08-03 11:25:47 +0530

    Revert "fix: generate multi-resolution 32x32/16x16 PNG & ICO favicons and remove dynamic canvas overwrite script"
    This reverts commit 4c684120017927b2c57c1eddde8e6854381db732.

--- dashboard_client/src/routes/__root.tsx
+++ dashboard_client/src/routes/__root.tsx
@@ -104,10 +104,8 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="/images/green_waveform_pure_transparent.png" />
+        <link rel="apple-touch-icon" href="/images/green_waveform_pure_transparent.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=IBM+Plex+Mono:wght@400;500&display=swap" rel="stylesheet" />
