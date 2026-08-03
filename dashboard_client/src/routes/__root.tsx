89d3fe1346140002360233ace0ab1ad70137d721
Vrushabh Gamare <vrushabhgamare@Vrushabhs-MacBook-Air-2.local>
2026-08-03 11:12:03 +0530

    fix: update favicon to crisp transparent green icon & apply KZUNO brand typography (Montserrat headings, Inter body text)

--- dashboard_client/src/routes/__root.tsx
+++ dashboard_client/src/routes/__root.tsx
@@ -108,7 +108,7 @@ function RootShell({ children }: { children: ReactNode }) {
         <link rel="apple-touch-icon" href="/images/green_waveform_pure_transparent.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,400;12..96,600;12..96,700;12..96,800&family=Instrument+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&family=IBM+Plex+Mono:wght@400;500&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=IBM+Plex+Mono:wght@400;500&display=swap" rel="stylesheet" />
         <script dangerouslySetInnerHTML={{ __html: `
           (function () {
             try {
